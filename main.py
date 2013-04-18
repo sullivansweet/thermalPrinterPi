13e60ae0d30f499ea9fa8e3fcc95fb067204b856
Sullivan <sullivansweet11@gmail.com>
2013-04-17 23:20:36 -0500

    changed startup
    Instead of running forecast.py on startup, it now runs timetemp.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ printer      = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
 # Called once per day (6:30am by default).
 # Invokes weather forecast and sudoku-gfx scripts.
 def daily():
-  subprocess.call(["python", "forecast.py"])
+  subprocess.call(["python", "timetemp.py"])
   subprocess.call(["python", "sudoku-gfx.py"])
 
 
